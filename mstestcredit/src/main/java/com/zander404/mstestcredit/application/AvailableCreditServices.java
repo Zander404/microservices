37ca29b89cbc67250803e44b526bf2baffebde0f
xandy <>
2025-02-17 16:48:03 -0400

    fix: minor changes

--- mstestcredit/src/main/java/com/zander404/mstestcredit/application/AvailableCreditServices.java
+++ mstestcredit/src/main/java/com/zander404/mstestcredit/application/AvailableCreditServices.java
@@ -6,13 +6,13 @@ import com.zander404.mstestcredit.domain.model.ClientSituation;
 import com.zander404.mstestcredit.domain.model.DataClient;
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AvailableCreditServices {
-
 
     private final ClientResourceClient clientClient;
     private final CardResourceClient cardClient;
@@ -29,6 +29,9 @@ public class AvailableCreditServices {
         }
         catch (FeignException.FeignClientException e) {
             int status = e.status();
+            if (HttpStatus.NOT_FOUND.value() == status) {
+                return null;
+            }
 
         }
     }
